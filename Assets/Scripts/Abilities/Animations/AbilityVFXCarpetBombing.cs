ab34307cf29712f4084c07ce95316e3e2a44ec1f
AnApostrophe <thestellaruniverse@gmail.com>
2025-10-22 14:21:35 -0700

    Carpet Bombing VFX

---
+++ Assets/Scripts/Abilities/Animations/AbilityVFXCarpetBombing.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class AbilityVFXCarpetBombing : AbilityVFX
+{
+    [SerializeField] float duration = 10f;
+    [SerializeField] int bombCount = 50;
+    [SerializeField] float bombRadius = 0.5f;
+    [SerializeField] float bombDelay = 0.05f;
+    [SerializeField] float bombScale = 1f;
+
+    [SerializeField] GameObject bombPrefab;
+
+    public override void Play()
+    {
+        StartCoroutine(CarpetBomb());
+        StartCoroutine(Destroy());
+    }
+
+    private IEnumerator CarpetBomb()
+    {
+        for (int i = 0; i < bombCount; i++)
+        {
+            GameObject bomb = Instantiate(bombPrefab, transform);
+            bomb.transform.localScale *= bombScale;
+            bomb.transform.Rotate(Vector3.forward, Random.Range(0, 360));
+            bomb.transform.Translate(Vector2.right * Random.Range(0, bombRadius));
+            yield return new WaitForSeconds(bombDelay);
+        }
+    }
+
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(duration);
+        StopAllCoroutines();
+        Destroy(gameObject);
+    }
+}
