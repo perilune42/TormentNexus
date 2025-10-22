e41431bc5b467a79fb0ef1ced4682717c7c60674
Chopsticksman <jameszhang345358@gmail.com>
2025-10-21 23:01:29 -0700

    new main menu scene

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,14 @@ public class MainMenu : MonoBehaviour
     public Button playButton;
     public Button quitButton;
     public Button mongoliaButton;
+    public Button mingButton;
+    public Button austriaButton;
+    public Button eafButton;
+    public Faction mongoliaFaction;
+    public Faction mingFaction;
+    public Faction austriaFaction;
+    public Faction eafFaction;
+
 
     private void Start()
     {
@@ -15,6 +23,13 @@ public class MainMenu : MonoBehaviour
         playButton.gameObject.SetActive(true);
         quitButton.gameObject.SetActive(true);
         mongoliaButton.gameObject.SetActive(false);
+        austriaButton.gameObject.SetActive(false);
+        mingButton.gameObject.SetActive(false);
+        eafButton.gameObject.SetActive(false);
+        mongoliaButton.GetComponentInChildren<TMP_Text>().text = mongoliaFaction.FactionName;
+        austriaButton.GetComponentInChildren<TMP_Text>().text = austriaFaction.FactionName;
+        mingButton.GetComponentInChildren<TMP_Text>().text = mingFaction.FactionName;
+        eafButton.GetComponentInChildren<TMP_Text>().text = eafFaction.FactionName;
     }
     public void OnPlayClick()
     {
@@ -23,11 +38,35 @@ public class MainMenu : MonoBehaviour
         playButton.gameObject.SetActive(false);
         quitButton.gameObject.SetActive(false);
         mongoliaButton.gameObject.SetActive(true);
+        austriaButton.gameObject.SetActive(true);
+        mingButton.gameObject.SetActive(true);
+        eafButton.gameObject.SetActive(true);
     }
 
     public void OnQuitClick()
     {
         Debug.Log("Game Quit");
         Application.Quit();
+    }
+
+    public void OnMongoliaClick()
+    {
+        Debug.Log("Mongolia Selected");
+
+    }
+
+    public void OnMingClick()
+    {
+        Debug.Log("Ming Selected");
+    }
+
+    public void OnAustriaClick()
+    {
+        Debug.Log("Austria Selected");
+    }
+
+    public void OnEAFClick()
+    {
+        Debug.Log("EAF Selected");
     }
 }
